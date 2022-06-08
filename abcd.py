7fd68c67cf4e4936a925c99e4936b83764db9922
amirahadlina <107099161+amirahadlina@users.noreply.github.com>
2022-06-08 14:59:42 +0800

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -9,6 +9,7 @@ st.title("This is a title")
 st.write("""
 # This is a first-level heading
 ## This is a second-level heading
+Hello! my name is Adlina, this is my first web app :)
 """)
 
 st.write(pd.DataFrame({
